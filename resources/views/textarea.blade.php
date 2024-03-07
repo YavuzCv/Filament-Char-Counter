90ab75e2bb6b7af50c54ad50cbb93601dd1f5767
yavuzcvlk <yavuzcivelek@hotmail.com.tr>
2024-03-07 16:05:43 +0300

    Filament V3

--- resources/views/textarea.blade.php
+++ resources/views/textarea.blade.php
@@ -1,57 +1,87 @@
+@php
+    use Filament\Support\Facades\FilamentView;
+
+    $hasInlineLabel = $hasInlineLabel();
+    $isConcealed = $isConcealed();
+    $isDisabled = $isDisabled();
+    $rows = $getRows();
+    $shouldAutosize = $shouldAutosize();
+    $statePath = $getStatePath();
+
+    $initialHeight = (($rows ?? 2) * 1.5) + 0.75;
+@endphp
+
 <x-dynamic-component
-        :component="$getFieldWrapperView()"
-        :id="$getId()"
-        :label="$getLabel()"
-        :label-sr-only="$isLabelHidden()"
-        :helper-text="$getHelperText()"
-        :hint="$getHint()"
-        :hint-action="$getHintAction()"
-        :hint-color="$getHintColor()"
-        :hint-icon="$getHintIcon()"
-        :required="$isRequired()"
-        :state-path="$getStatePath()"
+    :component="$getFieldWrapperView()"
+    :field="$field"
+    :has-inline-label="$hasInlineLabel"
 >
+    <x-slot
+        name="label"
+        @class([
+            'sm:pt-1.5' => $hasInlineLabel,
+        ])
+    >
+        {{ $getLabel() }}
+    </x-slot>
+
+    <x-filament::input.wrapper
+        :disabled="$isDisabled"
+        :valid="! $errors->has($statePath)"
+        :attributes="
+            \Filament\Support\prepare_inherited_attributes($getExtraAttributeBag())
+                ->class(['fi-fo-textarea overflow-hidden'])
+        "
+    >
+
     <div class="relative overflow-hidden"
          x-data="{characterLimit: {{ $getCharacterLimit() }}, characterCount: {{ mb_strlen($getState() ? $getState() : '') }}}">
         <div class="bg-white dark:bg-gray-700 absolute right-1 rtl:!left-1 rtl:!right-auto px-2 bottom-1 pb-1 text-sm" @if($getCharacterLimit()) :class="{'text-danger-500': characterCount > {{ $getCharacterLimit() }}}" @endif>
             <span x-text="characterCount"></span>@if($getCharacterLimit())/{{ $getCharacterLimit() }}@endif
         </div>
 
+                
         <textarea
-                @keyup="characterCount = $event.target.value.length"
-                {!! ($autocapitalize = $getAutocapitalize()) ? "autocapitalize=\"{$autocapitalize}\"" : null !!}
-                {!! ($autocomplete = $getAutocomplete()) ? "autocomplete=\"{$autocomplete}\"" : null !!}
-                {!! $isAutofocused() ? 'autofocus' : null !!}
-                {!! ($cols = $getCols()) ? "cols=\"{$cols}\"" : null !!}
-                {!! $isDisabled() ? 'disabled' : null !!}
-                id="{{ $getId() }}"
-                dusk="filament.forms.{{ $getStatePath() }}"
-        {!! ($placeholder = $getPlaceholder()) ? "placeholder=\"{$placeholder}\"" : null !!}
-        {!! ($rows = $getRows()) ? "rows=\"{$rows}\"" : null !!}
-        {{ $applyStateBindingModifiers('wire:model') }}="{{ $getStatePath() }}"
-        @if (! $isConcealed())
-            {!! filled($length = $getMaxLength()) ? "maxlength=\"{$length}\"" : null !!}
-            {!! filled($length = $getMinLength()) ? "minlength=\"{$length}\"" : null !!}
-            {!! $isRequired() ? 'required' : null !!}
-        @endif
-        {{
-            $attributes
-                ->merge($getExtraAttributes())
-                ->merge($getExtraInputAttributeBag()->getAttributes())
-                ->class([
-                    'filament-forms-textarea-component block w-full transition duration-75 rounded-lg shadow-sm focus:border-primary-500 focus:ring-1 focus:ring-inset focus:ring-primary-500 disabled:opacity-70',
-                    'dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:border-primary-500' => config('forms.dark_mode'),
-                    'border-gray-300' => ! $errors->has($getStatePath()),
-                    'border-danger-600 ring-danger-600' => $errors->has($getStatePath()),
-                    'dark:border-danger-400 dark:ring-danger-400' => $errors->has($getStatePath()) && config('forms.dark_mode')
-                ])
-        }}
-        @if ($shouldAutosize())
-            x-data="textareaFormComponent()"
-            x-on:input="render()"
-            style="height: 150px"
-            {{ $getExtraAlpineAttributeBag() }}
-        @endif
+            @keyup="characterCount = $event.target.value.length"
+            @if ($shouldAutosize)
+                @if (FilamentView::hasSpaMode())
+                    ax-load="visible"
+                @else
+                    ax-load
+                @endif
+                ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('textarea', 'filament/forms') }}"
+                x-data="textareaFormComponent({ initialHeight: @js($initialHeight) })"
+                x-ignore
+                x-intersect.once="render()"
+                x-on:input="render()"
+                x-on:resize.window="render()"
+                {{ $getExtraAlpineAttributeBag() }}
+            @endif
+            {{
+                $getExtraInputAttributeBag()
+                    ->merge([
+                        'autocomplete' => $getAutocomplete(),
+                        'autofocus' => $isAutofocused(),
+                        'cols' => $getCols(),
+                        'disabled' => $isDisabled,
+                        'id' => $getId(),
+                        'maxlength' => (! $isConcealed) ? $getMaxLength() : null,
+                        'minlength' => (! $isConcealed) ? $getMinLength() : null,
+                        'placeholder' => $getPlaceholder(),
+                        'readonly' => $isReadOnly(),
+                        'required' => $isRequired() && (! $isConcealed),
+                        'rows' => $rows,
+                        $applyStateBindingModifiers('wire:model') => $statePath,
+                    ], escape: false)
+                    ->class([
+                        'block w-full border-none bg-transparent px-3 py-1.5 text-base text-gray-950 placeholder:text-gray-400 focus:ring-0 disabled:text-gray-500 disabled:[-webkit-text-fill-color:theme(colors.gray.500)] disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.400)] dark:text-white dark:placeholder:text-gray-500 dark:disabled:text-gray-400 dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)] sm:text-sm sm:leading-6',
+                        'resize-none' => $shouldAutosize,
+                    ])
+                    ->style([
+                        "height: {$initialHeight}rem" => $shouldAutosize,
+                    ])
+            }}
         ></textarea>
     </div>
+    </x-filament::input.wrapper>
 </x-dynamic-component>
